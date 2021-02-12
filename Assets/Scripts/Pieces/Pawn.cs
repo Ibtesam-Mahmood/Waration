8a9a5f05c0c9b52a719cdcc7322582740ffef5a5
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2021-02-12 15:57:24 -0500

    peice class aded

---
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : Piece
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override List<Vector2> getMovement()
+    {
+        //List<Vector2> locations = new List<Vector2>();
+        return null;
+        //Vector2 move = tile;
+        //move.x++;
+        //locations.Add(tile);
+
+        //Vector2 move = tile;
+        //tile.x++;
+        //locations.Add(tile);
+    }
+}
---
+++ Assets/Scripts/Pieces/Piece.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Piece : MonoBehaviour
+{
+
+    ///The tile position of the piece
+    public Vector2 tile;
+
+    public abstract List<Vector2> getMovement();
+}
